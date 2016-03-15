60c4327d79a85705cf47c1a89990502850f88a61
pinakinmistry <pinakin.nmistry@gmail.com>
2016-03-15 13:56:07 +0530

    Creating url endpoints

--- index.js
+++ index.js
@@ -3,20 +3,12 @@ var fs = require('fs');
 var app = express();
 var multer = require('multer');
 var morgan = require('morgan');
+var path = require('path');
 var exec = require( 'child_process' ).exec;
 var port = 4000;
 
 app.use(express.static('client'));
 app.use(morgan('dev'));
-
-
-app.get('/', function (req, res) {
-	res.sendFile(__dirname, '/index.html', function (err) {
-		if(err) {
-			res.status(500).send(err);
-		}
-	});
-});
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -32,9 +24,25 @@ var upload = multer({
   limits: {fileSize: 1000000, files:1}
 });
 
+app.get('/upload/spijefie', function (req, res) {
+	res.sendFile(path.join(__dirname + '/client/upload.html'), function (err) {
+		if(err) {
+			res.status(500).send(err);
+		}
+	});
+});
+
 //handle posts on file uploads
 app.post('/pictures/upload', upload.single('image'), function (req, res) {
 	res.send();
+});
+
+app.get('/home', function (req, res) {
+	res.sendFile(path.join(__dirname + '/client/home.html'), function (err) {
+		if(err) {
+			res.status(500).send(err);
+		}
+	});
 });
 
 //wipe out all files from this location at regular interval
@@ -42,7 +50,7 @@ setInterval(function () {
 	exec( 'rm -rf ' + './public/uploads/*', function ( err, stdout, stderr ){
 	  console.log('Files deleted');
 	});
-}, 8 * 1000);
+}, 10 * 1000);
 
 app.listen(port, function () {
 	console.log('Listening on port', port);

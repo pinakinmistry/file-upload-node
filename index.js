98301f0dd0915df28c6e9a7906a3ca263f71a619
pinakinmistry <pinakin.nmistry@gmail.com>
2016-03-17 14:10:21 +0530

    current site added

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var path = require('path');
 var exec = require( 'child_process' ).exec;
 var port = 4000;
 
-app.use(express.static(__dirname + '/client'));
 app.use(morgan('dev'));
+app.use(express.static(__dirname + '/client'));
 app.use(express.static(__dirname + '/public'));
 
 var storage = multer.diskStorage({
@@ -33,13 +33,20 @@ app.get('/upload/spijefie', function (req, res) {
 	});
 });
 
-//handle posts on file uploads
 app.post('/pictures/upload', upload.array('image', 12), function (req, res) {
 	res.send();
 });
 
 app.get('/home', function (req, res) {
 	res.sendFile(path.join(__dirname + '/client/home.html'), function (err) {
+		if(err) {
+			res.status(500).send(err);
+		}
+	});
+});
+
+app.get('/spicejet', function (req, res) {
+	res.sendFile(path.join(__dirname + '/client/spicejet.html'), function (err) {
 		if(err) {
 			res.status(500).send(err);
 		}
@@ -62,7 +69,6 @@ function getWinners(dir, files_){
     return files_;
 }
 
-//console.log(getFiles('public/winners'))
 app.get('/winners', function (req, res) {
 	res.json(getWinners('public/winners'));
 });
@@ -77,4 +83,3 @@ app.get('/winners', function (req, res) {
 app.listen(port, function () {
 	console.log('Listening on port', port);
 });
-//client pulls the selected files and loads them in a banner
